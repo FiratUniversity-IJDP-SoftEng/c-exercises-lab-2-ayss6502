15564d3edeb3e44a607ffead2cf5adb41cba3eac
ayss6502 <aa.seednz2323@gmail.com>
2025-12-07 01:02:08 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void){
+
+    int n = get_int("Please enter a number between 1-50 \n");
+
+    unsigned long long factorial = 1;
+
+    int m = 1;
+
+    while(m <= n){
+        factorial *= m;
+        m++;
+    }
+    printf("%i! = %llu\n",n,factorial);
+
+    if(n > 20){
+        printf("There may be errors in the calculations for numbers greater than 20!\n");
+    }
+    return 0;
+}
