44b129b4da5fc1fb5f8e301ef2a080e0a0635d33
ayss6502 <aa.seednz2323@gmail.com>
2025-12-07 00:15:03 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#define size 10
 
+int main(void){
+  int numbers[size];
+  long sum=0;
+  printf("The numbers: ");
+  for(int i=0; i<size; i++){
+    scanf("%d",&numbers[i]);
+    sum += numbers[i];
+    printf("%i,",numbers[i]);
+  }
+  printf("\n");
+  printf("The sum of numbers: %ld \n", sum);
+
+  double avarage = sum/size;
+  printf("The avarage of the numbers: %.2f \n");
+
+  return 0;
+}
