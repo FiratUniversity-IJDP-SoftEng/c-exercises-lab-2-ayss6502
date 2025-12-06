75cae7883b47a8ab75aa005185bec509e66f920f
ayss6502 <aa.seednz2323@gmail.com>
2025-12-07 00:20:29 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void){
+
+  int numbers = 0;
+  long sum = 0;
+  int the numbers;
+
+  while(true){
+    thenumbers = get_int("Please enter the numbers: \n");
+
+    if(thenumbers ==-1){
+      break;
+    }
+    sum += thenumbers;
+    numbers++;
+  }
+  double avarage = (double) sum/numbers;
+  printf("The avarage is: \n");
+
+  return 0;
+}
