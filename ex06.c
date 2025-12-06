824ec944cd9adaf4470e2f653a5cd6e48552ce15
ayss6502 <aa.seednz2323@gmail.com>
2025-12-07 00:04:21 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void){
+  for(int i=100; i>=50; i--){
+    if(i%3==0){
+      printf("%i, ",i);
+    }
+    if(i==51){
+      printf("%i",i);
+    }
+  }
+  printf("\n");
+
+  return 0;
+}
