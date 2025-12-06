22db8acba77a480d45aa4801ec7bdf3b9f2d0a00
ayss6502 <aa.seednz2323@gmail.com>
2025-12-07 00:31:19 +0300

    Update ex11.c

--- ex11.c
+++ ex11.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(void){
+
+    int n=5;
+
+    for(int i =0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==j || i+j==4){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
